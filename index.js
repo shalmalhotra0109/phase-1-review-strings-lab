9e2c3bd38db650390123214332a6c193eb5ae06e
Shal Malhotra <smalhotra0109@gmail.com>
2023-10-02 12:21:48 -0400

    Strings Lab changes

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Shal Malhotra'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
